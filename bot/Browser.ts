bba207179717395f8f685f30809e95b8eb8b307e
mamii. <70213359+mamiiblt@users.noreply.github.com>
2026-04-21 01:17:03 +0300

    feat: Add executablePath and args to puppeteer launch

--- bot/Browser.ts
+++ bot/Browser.ts
@@ -6,6 +6,8 @@ export async function getBrowser() {
     if (!browser) {
         browser = await puppeteer.launch({
             headless: true,
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
         });
     }
     return browser;
